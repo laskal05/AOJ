fa3821f5af5960f245a3bc4e2a59d74f463cbbb4
noopy05 <hrh.syn.mnmk.lb@gmail.com>
2013-03-31 14:55:49 +0900

    Capitalize

---
+++ 0020.cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+int main(void)
+{
+    int i = 0, j;
+    char n;
+    char a[10000] = { 0 };
+    while( scanf("%c",&n) == 1)
+    {
+        int flag1 = 0,flag2 = 0;
+        if( !flag2 )
+        {
+            if( 'A' <= n && n <= 'Z' )
+            {
+                n += 32;
+                flag1++;
+            }
+        }
+        if( !flag1 )
+        {
+            if( 'a' <= n && n <= 'z' )
+                n -= 32;
+            flag2++;
+        }
+        a[i] = n;
+        i++;
+    }
+    printf("%s",a);
+    return(0);
+}
